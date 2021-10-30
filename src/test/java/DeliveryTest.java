3c0d3ae310eda574f793ad115bdcd3c67dee5f92
Екатерина <laufeyconyy@gmail.com>
2021-10-30 17:15:41 +0300

    remake

--- src/test/java/DeliveryTest.java
+++ src/test/java/DeliveryTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
@@ -14,12 +16,15 @@ public class DeliveryTest {
         open("http://localhost:9999");
         $("[placeholder='Город']").setValue("Нижний Новгород");
         $("[type='tel']").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[type='tel']").setValue("15112021");
+        String today = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[type='tel']").setValue(today);
         $("[name='name']").setValue("Екатерина Агапова");
         $("[name='phone']").setValue("+70987654321");
         $("[data-test-id=agreement]").click();
         $(withText("Забронировать")).click();
         $(withText("Успешно!")).shouldBe(appear, Duration.ofSeconds(15));
+        $(withText("Встреча успешно забронирована на")).shouldBe(appear);
+        $(withText(today)).shouldBe(appear);
 
     };
 }
